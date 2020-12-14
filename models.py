15b9a5fe7d58be00c7d9d89852003a56b7d08b00
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-12-14 11:49:48 +0200

    Added __repr__ to Company

--- models.py
+++ models.py
@@ -5,3 +5,6 @@ class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     data = db.Column(db.JSON)
+
+    def __repr__(self):
+        return "<Company {}>".format(self.name)
