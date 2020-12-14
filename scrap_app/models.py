37e8ecaea6822113c9dc23ae531ece92dc06feef
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-12-14 17:23:59 +0200

    Added field created to db

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -1,10 +1,12 @@
 from scrap_app.extensions import db
+from datetime import datetime
 
 
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     data = db.Column(db.JSON)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return "<Company {}>".format(self.name)
